34c950f5aa84218c27437132d5cfba17883bbd94
w3qiu <damon.q26@gmail.com>
2019-07-18 22:04:53 -0700

    Initial Commit

---
+++ TinderFireStore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TinderFireStore
+//
+//  Created by wenlong qiu on 7/18/19.
+//  Copyright © 2019 wenlong qiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
