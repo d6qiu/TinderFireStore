3d2dceb86ab68930a28f8af26d92fdc2cde824e9
w3qiu <damon.q26@gmail.com>
2019-11-04 11:31:17 -0800

    add boba text containers in single chat

--- TinderFireStore/Controllers/Inbox/SingleChatController.swift
+++ TinderFireStore/Controllers/Inbox/SingleChatController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 struct Message {
     let text: String
+    let isUserText: Bool
 }
 
 class MessageCell: ListCell<Message> {
+    
+    let textBoba = UIView(backgroundColor: #colorLiteral(red: 0.8862745098, green: 0.8862745098, blue: 0.8862745098, alpha: 1), opacity: 1)
+    
+    var anchoredConstraints: AnchoredConstraints!
+    
     
     //uilabels are vertical center aligned.
     let textView : UITextView = {
@@ -27,13 +33,34 @@ class MessageCell: ListCell<Message> {
     override var item: Message! {
         didSet {
             textView.text =  item.text
+            if item.isUserText {
+                //stick to right
+                self.anchoredConstraints.trailing?.isActive = true
+                self.anchoredConstraints.leading?.isActive = false
+                textBoba.backgroundColor = #colorLiteral(red: 0, green: 0.7427282333, blue: 1, alpha: 1)
+                textView.textColor = .white
+            } else {
+                //stick to left
+                self.anchoredConstraints.trailing?.isActive = false
+                self.anchoredConstraints.leading?.isActive = true
+                textBoba.backgroundColor = #colorLiteral(red: 0.8861967325, green: 0.8863244653, blue: 0.8861687779, alpha: 1)
+                textView.textColor = .black
+            }
         }
     }
     
     override func setupViews() {
         super.setupViews()
-        addSubview(textView)
-        textView.fillSuperview()
+        addSubview(textBoba)
+        textBoba.layer.cornerRadius = 12
+        self.anchoredConstraints = textBoba.anchor(top: topAnchor, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor)
+        self.anchoredConstraints.leading?.constant = 20 //padding 20 from left
+        self.anchoredConstraints.trailing?.constant = -20// padding 20 from right
+        
+        
+        self.textBoba.widthAnchor.constraint(lessThanOrEqualToConstant: 250).isActive = true //return the max width the textView will use
+        self.textBoba.addSubview(textView)
+        self.textView.fillSuperview(padding: .init(top: 4, left: 12, bottom: 4, right: 12))
     }
     
     
@@ -60,12 +87,17 @@ class SingleChatController: ListController<MessageCell, Message>, UICollectionVi
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        collectionView.alwaysBounceVertical = true //boucing animation when scroll down to end
         view.addSubview(singleChatNavBar)
         singleChatNavBar.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, size: .init(width: 0, height: navBarHeight))
         collectionView.contentInset.top = navBarHeight //move collection view's content down to leave space for messageNavBar
+        collectionView.scrollIndicatorInsets.top = navBarHeight //shift scroll indicator down so it aligns with collection view content
         
         singleChatNavBar.backButton.addTarget(self, action: #selector(handleBack), for: .touchUpInside)
+        
+        let statusBarCover = UIView(backgroundColor: .white, opacity: 1)
+        view.addSubview(statusBarCover)
+        statusBarCover.anchor(top: view.topAnchor, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.topAnchor, trailing: view.trailingAnchor)
     }
     
     @objc fileprivate func handleBack() {
